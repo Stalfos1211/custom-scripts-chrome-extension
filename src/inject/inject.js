7662f041d388801c50c584cf30dc1a28cffe48ec
NZXT-HYBRID\NZXT-Hybrid <stalfos1211@gmail.com>
2017-07-22 17:13:44 -0500

    auto run scripts first try

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -6,20 +6,47 @@ chrome.extension.sendMessage({}, function(response) {
 
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
+		//console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
 
-		runAutoScripts();
+		getAutoScripts();
 
 	}
 	}, 10);
 });
 
-function runAutoScripts () {
+function runAutoScripts (scripts) {
+	
+	for (script in scripts) {
+		let url = scripts[script].url;
+		let code = scripts[script].code;
 
-	// Retrieve codes that needs to be auto ran
-	chrome.storage.local.get("scripts", (result) => {
-				console.log('get from storage result this is the inject script:', result.scripts)
-	})
+		if (location.href.indexOf(url) > -1) {		
+			eval(code);
+		}
+	}
+
+}
+
+function getAutoScripts () {
+
+	let autoRunScripts = [];
+
+	new Promise((resolve) => {
+
+		// Retrieve codes that needs to be auto ran
+		chrome.storage.local.get("scripts", (result) => {
+					let scripts = result.scripts;
+
+					// Get auto run codes and push them into autoRunScripts array formatted {url: 'http', code: '//code'}
+					for (script in scripts) {
+						if (scripts[script].url) {
+							autoRunScripts.push({'url': scripts[script].url, 'code': scripts[script].code})
+						}
+					}
+					resolve(runAutoScripts(autoRunScripts));
+		})
+	}) 
+
 }
 // http://unbill.us/assets/images/subpage-bg-q2.jpg
